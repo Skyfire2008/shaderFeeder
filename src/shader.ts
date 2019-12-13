f1980306952bc79429dc03b9c956350f601adf6b
Leonid Darovskikh <leonid.darovskikh@isis-papyrus.com>
2019-12-13 15:55:39 +0100

    still working on shader

--- src/shader.ts
+++ src/shader.ts
@@ -1,8 +1,15 @@
 export class Shader {
 
+	//STATIC PROPERTIES AND METHODS
 	private static gl: WebGLRenderingContext;
 	private static vert: WebGLShader;
 
+	/**
+	 * Loads a given shader
+	 * @param src shader source code
+	 * @param type shader type
+	 * @returns shader handle
+	 */
 	static load(src: string, type: number): WebGLShader {
 		const id = Shader.gl.createShader(type);
 		Shader.gl.shaderSource(id, src);
@@ -21,7 +28,28 @@ export class Shader {
 		Shader.vert = Shader.load(vertSrc, gl.VERTEX_SHADER);
 	}
 
-	constructor(fragSrc: string) {
+	//INSTANCE PROPERTIES AND METHODS
 
+	private id: WebGLProgram;
+	private frag: WebGLShader;
+	private params: Map<string, Param>;
+
+	constructor(fragSrc: string) {
+		this.id = Shader.gl.createProgram();
+
+		this.frag = Shader.load(fragSrc, Shader.gl.FRAGMENT_SHADER);
+
+		Shader.gl.attachShader(this.id, Shader.vert);
+		Shader.gl.attachShader(this.id, this.frag);
+		Shader.gl.linkProgram(this.id);
+		Shader.gl.validateProgram(this.id);
+
+		if (!Shader.gl.getProgramParameter(this.id, Shader.gl.LINK_STATUS)) {
+			throw "Error while linking the program: " + Shader.gl.getProgramInfoLog(this.id);
+		}
 	}
+}
+
+interface Param {
+
 }
