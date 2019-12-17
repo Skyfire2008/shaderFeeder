8b55667a95ffe2590130974dd1c1ce6ee524825a
Leonid Darovskikh <skyfire2008@gmail.com>
2019-12-17 06:55:25 +0100

    file upload working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
 namespace ShaderFeeder {
 
+	interface NamedImage {
+		image: HTMLImageElement;
+		name: string;
+	}
+
+	class ViewModel {
+		public images: KnockoutObservableArray<NamedImage>;
+
+		constructor() {
+			this.images = ko.observableArray();
+		}
+
+		public uploadFile(file: File) {
+			const fr = new FileReader();
+			fr.onload = (e: any) => {
+				const img = new Image();
+				img.onload = (e: Event) => {
+					this.images.push({
+						name: file.name,
+						image: img
+					});
+
+				};
+
+				img.src = <string>e.target.result;
+			};
+
+			fr.readAsDataURL(file);
+		}
+	}
+
 	window.addEventListener("load", () => {
-		ko.applyBindings();
+		ko.applyBindings(new ViewModel());
 	});
 }
